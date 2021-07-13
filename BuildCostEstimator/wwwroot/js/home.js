345e2296b3ec37fa7ec16be9ba3b0e73152fbeef
tim <no@email.com>
2021-07-13 15:36:33 -0700

    Implemented dynamic 'Go'/'Test' submit button.

---
+++ BuildCostEstimator/wwwroot/js/home.js
@@ -0,0 +1,22 @@
+﻿$('#pastebinUrlInputForm').on('input',function() {
+  
+
+    if ($('#pastebinUrlInputForm').val() != "") {
+        $('#pastebinSubmitButton').text('Go');
+      
+    } else {
+        $('#pastebinSubmitButton').text('Test');
+        
+    }
+
+});
+
+$('#pastebinSubmitButton').click(function() {
+
+
+    if ($('#pastebinUrlInputForm').val() == "") {
+        $('#pastebinUrlInputForm').val('https://pastebin.com/7nhJbxk4');
+    } 
+    
+});
+
