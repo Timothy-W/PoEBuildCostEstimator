1c745667ade8dffe62dd4044bab370816614a32a
tim <no@email.com>
2021-07-13 14:19:28 -0700

    Commented code and replaced with simple message. Revert when we get captcha working.

--- BuildCostEstimator/wwwroot/js/feedback.js
+++ BuildCostEstimator/wwwroot/js/feedback.js
@@ -6,20 +6,32 @@ $(document).ready(() => {
 });
 
 async function GetFeedback() {
-    const { value: text } = await Swal.fire({
-        input: 'textarea',
-        inputLabel: 'Feedback',
-        inputPlaceholder: 'Type your feedback here...',
-        inputAttributes: {
-            'aria-label': 'Type your message here'
-        },
-        confirmButtonText: 'Submit',
-        showCancelButton: true
+    
+    Swal.fire({
+        text: "Please submit feedback to toxxindev@gmail.com",
+        confirmButtonColor: '#f89406',
     });
+    
+    //const { value: text } = await Swal.fire({
+    //    input: 'textarea',
+    //    inputLabel: 'Feedback',
+    //    inputPlaceholder: 'Type your feedback here...',
+    //    inputAttributes: {
+    //        'aria-label': 'Type your message here'
+    //    },
+    //    confirmButtonText: 'Submit',
+    //    showCancelButton: true
+    //});
 
-    if (text) {
-        Swal.fire({
-            text: "Feedback submitted, thank you!",
-        });
-    }
+    //if (text) {
+    //    Swal.fire({
+    //        text: "Feedback submitted, thank you!",
+    //    });
+    //}
+}
+
+async function SubmitFeedback() {
+
+
+
 }
