2fc4b1f3040e33e42bd030f62d8bf588f21ea6c2
tim <no@email.com>
2021-07-13 13:01:39 -0700

    Added feedback button to nav bar and input alert.

---
+++ BuildCostEstimator/wwwroot/js/feedback.js
@@ -0,0 +1,25 @@
+﻿
+
+
+$(document).ready(() => {
+    $('#MyButton').click(() => GetFeedback());
+});
+
+async function GetFeedback() {
+    const { value: text } = await Swal.fire({
+        input: 'textarea',
+        inputLabel: 'Feedback',
+        inputPlaceholder: 'Type your feedback here...',
+        inputAttributes: {
+            'aria-label': 'Type your message here'
+        },
+        confirmButtonText: 'Submit',
+        showCancelButton: true
+    });
+
+    if (text) {
+        Swal.fire({
+            text: "Feedback submitted, thank you!",
+        });
+    }
+}
