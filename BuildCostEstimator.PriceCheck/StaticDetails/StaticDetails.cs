3c035be1aa2a100c3fc3c60292616c8c79083dcc
tim <no@email.com>
2021-08-01 14:03:00 -0700

    Changed league to Expedition

--- BuildCostEstimator.PriceCheck/StaticDetails/StaticDetails.cs
+++ BuildCostEstimator.PriceCheck/StaticDetails/StaticDetails.cs
@@ -14,8 +14,8 @@ namespace BuildCostEstimator.PriceCheck
         public const string Rare = "Rare";
         public const string Unique = "Unique";
 
-        //public const string League = "Expedition";
-        public const string League = "Standard";
+        public const string League = "Expedition";
+        //public const string League = "Standard";
 
         // Poe Ninja url
         public const string PoeNinjaUrl = "https://poe.ninja/api/data/itemoverview?";
