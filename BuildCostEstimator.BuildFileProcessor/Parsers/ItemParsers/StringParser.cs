302069bbfea19b027606522869eab814f2efee81
tim <no@email.com>
2021-08-01 17:12:30 -0700

    Added StringParser and IntParser base classes

---
+++ BuildCostEstimator.BuildFileProcessor/Parsers/ItemParsers/StringParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using BuildCostEstimator.BuildFileProcessor.Parsers.Interfaces;
+
+namespace BuildCostEstimator.BuildFileProcessor.Parsers.ItemParsers
+{
+    public class StringParser : IParser<string>
+    {
+        public async Task<string> ParseAsync(XElement element)
+        {
+            return await Task.Run(() => Parse(element));
+        }
+
+        public virtual string Parse(XElement element)
+        {
+            return string.Empty;
+        }
+    }
+}
